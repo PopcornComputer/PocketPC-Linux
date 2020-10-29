b3cf594e2df3d2d88a591fc3d2fede426c61a7de
Ondrej Jirman <megous@megous.com>
2020-10-29 01:13:49 +0100

    drm/sun4i: Fix mipi-dsi panel framerate being 2/3 of the expected value
    This happens on Pinephone and on Pinetab. Both use MIPI-DSI panels.
    
    On Pinephone framerate is 36.6 instead of expected 55.
    On Pinetab the similar 2/3 ratio holds.
    
    Myabe there's a bug in BSP and dclk_div should really be 6 instead of 4.
    
    Signed-off-by: Ondrej Jirman <megous@megous.com>

--- drivers/gpu/drm/sun4i/sun4i_tcon.c
+++ drivers/gpu/drm/sun4i/sun4i_tcon.c
@@ -398,8 +398,8 @@ static void sun4i_tcon0_mode_set_cpu(struct sun4i_tcon *tcon,
 	u32 block_space, start_delay;
 	u32 tcon_div;
 
-	tcon->dclk_min_div = SUN6I_DSI_TCON_DIV;
-	tcon->dclk_max_div = SUN6I_DSI_TCON_DIV;
+	tcon->dclk_min_div = 6;
+	tcon->dclk_max_div = 6;
 
 	sun4i_tcon0_mode_set_common(tcon, mode);
 
