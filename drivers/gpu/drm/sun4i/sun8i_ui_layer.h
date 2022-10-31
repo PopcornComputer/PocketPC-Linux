76d5cad150c29be3a09e4df0328eb9fb8287260a
Ondrej Jirman <megi@xff.cz>
2022-10-31 03:23:11 +0100

    drm/sun4i: Mark one of the UI planes as a cursor one
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/gpu/drm/sun4i/sun8i_ui_layer.h
+++ drivers/gpu/drm/sun4i/sun8i_ui_layer.h
@@ -51,5 +51,7 @@ struct sun8i_layer;
 
 struct sun8i_layer *sun8i_ui_layer_init_one(struct drm_device *drm,
 					    struct sun8i_mixer *mixer,
-					    int index);
+					    int index,
+					    enum drm_plane_type type);
+
 #endif /* _SUN8I_UI_LAYER_H_ */
